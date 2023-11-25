71652eb212ee980d2ba6a7c7e81750328599f2a4
Christopher Rodriguez <chrisrod115@gmail.com>
2023-11-24 19:23:27 -0800

    added cart model

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart';
+
 
 function App() {
   const [cartIsShown,setCartIsShown] = useState(false);
@@ -16,6 +18,7 @@ function App() {
 
   return (
     <CartProvider>
+      <Cart />
       {cartIsShown && <div>Cart...</div>}
       <Header onShowCart={showCartHandler}/>
       <main>
